84d4a8320296dda0f8b61ab6898b62547007c805
marathonic <castleracer@outlook.com>
2022-08-04 14:41:14 -0500

    Cleans up unused code and begins React Router implementation

--- src/components/GameArea.js
+++ src/components/GameArea.js
@@ -3,105 +3,17 @@ export default function GameArea({
   showSelectionModal,
   openModal,
 }) {
-  //find:
-  //  Rogue, Martian Manhunter, Moon Knight, Ramona Flowers
-
-  // const getClickCoordinates = (e) => {
-  //   let rect = e.currentTarget.getBoundingClientRect();
-  //   // let x = e.clientX - rect.left;
-  //   let x = e.screenX;
-  //   let y = e.offsetY;
-  //   console.log(x);
-  //   console.log(`left: ${x} top: ${y}`);
-  // };
-
-  const testFunc = () => {
-    alert("Hello");
-  };
-
-  const getClickCoords = (e) => {
-    const xCoord = Math.round(
-      (e.nativeEvent.offsetX / e.nativeEvent.target.offsetWidth) * 100
-    );
-    const yCoord = Math.round(
-      (e.nativeEvent.offsetY / e.nativeEvent.target.offsetHeight) * 100
-    );
-    // console.log("X at: " + xCoord);
-    // console.log("Y at: " + yCoord);
-
-    // Another function, maybe this one's parent:
-
-    // const isMatch = () => {
-    // if X matches (falls in the range of) the location of Rogue's head, the following const is true.
-    const matchesX = xCoord >= 74 && xCoord <= 82;
-    const matchesY = yCoord >= 83 && xCoord <= 91;
-    // if (matchesX && matchesY) {
-    //   console.log("Rogue");
-    // }
-    // };
-  };
-
-  // Attempting to position modal at mouse click position:
-
-  // window.onload = function () {
-  //   var ImageMap = function (map, img) {
-  //       var n,
-  //         areas = map.getElementsByTagName("area"),
-  //         len = areas.length,
-  //         coords = [],
-  //         previousWidth = 856; // should be the original image width (856)
-  //       for (n = 0; n < len; n++) {
-  //         coords[n] = areas[n].coords.split(",");
-  //       }
-  //       this.resize = function () {
-  //         var n,
-  //           m,
-  //           clen,
-  //           x = img.offsetWidth / previousWidth;
-  //         for (n = 0; n < len; n++) {
-  //           clen = coords[n].length;
-  //           for (m = 0; m < clen; m++) {
-  //             coords[n][m] *= x;
-  //           }
-  //           areas[n].coords = coords[n].join(",");
-  //         }
-  //         previousWidth = document.body.clientWidth;
-  //         return true;
-  //       };
-  //       window.onresize = this.resize;
-  //     },
-  //     imageMap = new ImageMap(
-  //       document.getElementById("super-map-id"),
-  //       document.getElementById("img_ID")
-  //     );
-  //   imageMap.resize();
-  //   return;
-  // };
-
   return (
     <div className="game-area-container">
       <img
         id="img_ID"
         src="./superheroes-edit.jpg"
-        useMap="#superheroes-map"
+        // useMap="#superheroes-map"
         border="0"
         width={"100%"}
         alt=""
         onClick={clickPosition}
       />
-
-      {/* We're not using the map anymore */}
-      {/* <map id="super-map-id" name="superheroes-map"> */}
-      {/* <area */}
-      {/* shape="circle" */}
-      {/* coords="640,1790,77" //552, 1058 for Mjolnir, 640, 1790 for Elektra */}
-      {/* alt="test" */}
-      {/* href="#" */}
-      {/* title="title one" */}
-      {/* style={{ cursor: "pointer", border: "red" }} */}
-      {/* onClick={testFunc} */}
-      {/* ></area> */}
-      {/* </map> */}
     </div>
   );
 }
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -5,13 +5,10 @@ import { useState } from "react";
 export default function Play({
   openModal,
   setLastClickPosition,
-  lastClickPosition,
   setClientClickPosition,
-  lastFound,
   setLastFound,
   allPositions,
   previouslyFound,
-  setPreviouslyFound,
 }) {
   console.log(allPositions);
 
@@ -26,72 +23,26 @@ export default function Play({
     console.log("Coordinates clicked: ");
     console.log(xCoord, yCoord);
 
-    // we could bundle these into const rogue
-    // We want to rework this logic to work alongside our Firebase integration
-    // Loop over each object in our database, and
-    // if the click coordinates here match those values,
-    // get the [name] value, which will be 'rogue' in this case.
-    // reworked: const matchesX = xCoord >= myObj.xMin && myObj.xCoord <= xMax;
-    // reworked: const matchesY = yCoord >= myObj.yMin && yCoord <= myObj.yMax;
-    // reworked: if (matchesX && matchesY) {
-    // reworked: console.log(`You found ${myObj.name}!`); // <--- "You found rogue!"
-    // reworked: setLastFound(myObj.name); // <--- lastFound === 'rogue'
-    // reworked: }
-    // const matchesX = xCoord >= 74 && xCoord <= 82;
-    // const matchesY = yCoord >= 83 && xCoord <= 91;
-    // if (matchesX && matchesY) {
-    // console.log("You found Rogue!");
-    // setLastFound("rogue");
-    // IMPORTANT:
-    // Change lastFound below to be myObj.name;
-    // setPreviouslyFound(...previouslyFound, lastFound);
-    // }
-    //what we're trying to figure out:
-    // Take the click coordinates and check if it falls between the ranges
-    // of any 1 object inside of our database(allPositions).
-    // If so, console log the object's --name-- property.
     for (let i = 0; i < allPositions.length; i++) {
       const curr = allPositions[i];
-      // let found = "found";
-      // same as saying: if (xCoord => curr.xMin && xCoord <= curr.xMax) {
       if (curr.xMin <= xCoord && xCoord <= curr.xMax) {
         if (curr.yMin <= yCoord && yCoord <= curr.yMax) {
-          // WHY WE NEED LASTFOUND:
-          // We want lastFound because we need to ask the player:
-          // Who is that? / Who did you find?
-          // Then they'll pick one of the options, and that's where lastFound comes in,
-          // because we will check that the name of that clicked character is === lastFound
-          // all previously found characters will have their images greyed out
-          // if the clicked character is not yet part of previouslyFound, add it to previouslyFound.
-          // Alternatively, we could also display info about the character when the click coordinates match those of someone in previouslyFound.
-          // For example, if we find Rogue, and then we click on her again, we could get a modal that says:
-          // <img src= curr.headshot />, <h4>{curr.name}</h4> (same as: Rogue). <span>{curr.affiliation}</span>(same as: Affiliation: X-Men), description, etc.
           setLastFound(curr.name);
-          // found = curr.name;
-          // setPreviouslyFound((prevState) => {
-          //   const prev = prevState.filter((person) => person !== found);
-          //   return [...prev, found];
-          // });
         }
       }
-
-      // setPreviouslyFound();
     }
 
-    // lastClickPosition is used to place the modal
     setLastClickPosition({
       x: xCoord,
       y: yCoord,
     });
     openModal();
 
-    // new code:
     var rect = e.target.getBoundingClientRect(); // get some position, scale,... properties of the item
     let mousePos = { x: 0, y: 0 };
     mousePos.x = e.clientX - rect.left; // get the mouse position relative to the element
     mousePos.y = e.clientY - e.target.scrollTop; //
 
-    // test console log
     console.log(window.innerWidth);
     setClientClickPosition({ x: mousePos.x, y: mousePos.y });
   };
